39c8cb415532d4e829b1acc39d14a347d477405f
Grant <284885069@qq.com>
2024-11-05 18:24:34 +0800

    feat: add @tiptap/pm to build

--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -44,7 +44,7 @@ export default defineConfig({
                 "react-dom",
                 "react/jsx-runtime",
                 "@easykit/design",
-                ...Object.keys(pkg.dependencies)
+                ...Object.keys(pkg.dependencies).filter((key) => !["@tiptap/pm"].includes(key))
             ],
             output: {
                 entryFileNames: '[name].js',
