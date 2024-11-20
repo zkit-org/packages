911eb6bb18bee5dd910729f236f922b14e519fda
Grant <284885069@qq.com>
2024-11-20 17:46:37 +0800

    feat: 更新依赖版本

--- packages/design/src/app/demo/tree-select/page.tsx
+++ packages/design/src/app/demo/tree-select/page.tsx
@@ -10,10 +10,42 @@ const Page = () => {
         <TreeSelect
             loading={false}
             clearable={true}
-            placeholder={"请选择"}
             onChange={setValue}
             value={value}
-            treeData={[]}
+            treeData={[
+                {
+                    key: "1",
+                    title: "1",
+                    children: [
+                        {
+                            key: "1-1",
+                            title: "1-1",
+                        },
+                        {
+                            key: "1-2",
+                            title: "1-2",
+                        }
+                    ],
+                },
+                {
+                    key: "2",
+                    title: "2",
+                    children: [
+                        {
+                            key: "2-1",
+                            title: "2-1",
+                        },
+                        {
+                            key: "2-2",
+                            title: "2-2",
+                        }
+                    ],
+                },
+                {
+                    key: "3",
+                    title: "3"
+                }
+            ]}
         />
     </div>;
 }
