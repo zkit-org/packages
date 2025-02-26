e8f4545daaa51f228cef591f0f7345c981bb9124
Grant <284885069@qq.com>
2025-02-26 17:55:32 +0800

    feat: Empty 样式控制

--- packages/design/lib/components/uix/empty/index.tsx
+++ packages/design/lib/components/uix/empty/index.tsx
@@ -2,20 +2,29 @@ import {FC, ReactNode, useContext} from "react";
 import {IconEmpty} from "@arco-iconbox/react-atom-ui";
 import {UIXContext} from "@easykit/design/components/uix/config-provider";
 import get from "lodash/get";
+import classNames from "classnames";
 
 export type EmptyProps = {
   text?: ReactNode;
   icon?: ReactNode;
+  className?: string;
+  textClassName?: string;
+  iconClassName?: string;
 }
 
 export const Empty: FC<EmptyProps> = (props) => {
+  const {
+    className,
+    iconClassName,
+    textClassName,
+  } = props;
   const config = useContext(UIXContext);
   const text = props.text || get(config.locale, "Empty.text");
 
-  return <div className={"flex justify-center items-center flex-col p-4"}>
-    <div>
+  return <div className={classNames("flex justify-center items-center flex-col p-4", className)}>
+    <div className={iconClassName}>
       {props.icon || <IconEmpty className={"text-6xl text-muted-foreground opacity-60"}/>}
     </div>
-    <div className={"text-xs text-secondary"}>{text}</div>
+    <div className={classNames("text-xs text-secondary-foreground/70", textClassName)}>{text}</div>
   </div>
 }
