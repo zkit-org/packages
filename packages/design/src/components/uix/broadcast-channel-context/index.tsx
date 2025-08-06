44151d2d3d348fec8310bba9134e31acfcaa22f3
Grant <284885069@qq.com>
2025-08-06 16:06:48 +0800

    feat: add BroadcastChannelProvider

---
+++ packages/design/src/components/uix/broadcast-channel-context/index.tsx
@@ -0,0 +1,28 @@
+import { type FC, type PropsWithChildren, createContext, useContext, useEffect, useState } from 'react'
+
+export const BroadcastChannelContext = createContext<BroadcastChannel | null>(null)
+
+export type BroadcastChannelProviderProps = PropsWithChildren<{
+  channelName: string
+  onMessage: ((this: BroadcastChannel, ev: MessageEvent) => unknown) | null
+}>
+
+export const BroadcastChannelProvider: FC<BroadcastChannelProviderProps> = (props) => {
+  const { channelName, onMessage } = props
+  const [channel, setChannel] = useState<BroadcastChannel | null>(null)
+
+  useEffect(() => {
+    if (typeof BroadcastChannel === 'undefined') {
+      return
+    }
+    const channel = new BroadcastChannel(channelName)
+    channel.onmessage = onMessage
+    setChannel(channel)
+  }, [channelName, onMessage])
+
+  return <BroadcastChannelContext.Provider value={channel}>{props.children}</BroadcastChannelContext.Provider>
+}
+
+export const useBroadcastChannel = () => {
+  return useContext(BroadcastChannelContext)
+}
--- packages/design/src/index.ts
+++ packages/design/src/index.ts
@@ -7,26 +7,33 @@ export * from './lib'
 export * from './hooks'
 
 // base
-export {Toaster} from './components/ui/sonner';
-export {Input} from './components/ui/input';
-export {toast} from "sonner"
-export {ScrollArea, ScrollBar} from "./components/ui/scroll-area";
-export {Tabs, TabsContent, TabsList, TabsTrigger,} from "./components/ui/tabs"
+export { Toaster } from './components/ui/sonner'
+export { Input } from './components/ui/input'
+export { toast } from 'sonner'
+export { ScrollArea, ScrollBar } from './components/ui/scroll-area'
+export { Tabs, TabsContent, TabsList, TabsTrigger } from './components/ui/tabs'
 export {
-  Table, TableBody, TableCell, TableHead, TableHeader, TableRow, TableFooter, TableCaption
-} from "./components/ui/table";
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableFooter,
+  TableCaption,
+} from './components/ui/table'
 // biome-ignore lint/performance/noReExportAll: <explanation>
 export * from './components/ui/dropdown-menu'
-export {Separator} from "./components/ui/separator";
-export {Textarea} from "./components/ui/textarea";
-export {Badge} from "./components/ui/badge";
+export { Separator } from './components/ui/separator'
+export { Textarea } from './components/ui/textarea'
+export { Badge } from './components/ui/badge'
 export {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "./components/ui/popover";
-export {Calendar} from "./components/ui/calendar";
-export {Progress} from "./components/ui/progress";
+} from './components/ui/popover'
+export { Calendar } from './components/ui/calendar'
+export { Progress } from './components/ui/progress'
 export {
   Command,
   CommandEmpty,
@@ -36,7 +43,7 @@ export {
   CommandList,
   CommandSeparator,
   CommandShortcut,
-} from "./components/ui/command";
+} from './components/ui/command'
 export {
   Breadcrumb,
   BreadcrumbItem,
@@ -44,15 +51,17 @@ export {
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
-} from "./components/ui/breadcrumb"
+} from './components/ui/breadcrumb'
 export {
-  HoverCard, HoverCardTrigger, HoverCardContent
-} from "./components/ui/hover-card";
+  HoverCard,
+  HoverCardTrigger,
+  HoverCardContent,
+} from './components/ui/hover-card'
 export {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
-} from "./components/ui/resizable";
+} from './components/ui/resizable'
 export {
   Sheet,
   SheetClose,
@@ -62,12 +71,12 @@ export {
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-} from "./components/ui/sheet"
-export {Skeleton} from "./components/ui/skeleton";
+} from './components/ui/sheet'
+export { Skeleton } from './components/ui/skeleton'
 export {
   RadioGroup,
   RadioGroupItem,
-} from "./components/ui/radio-group";
+} from './components/ui/radio-group'
 // biome-ignore lint/performance/noReExportAll: <explanation>
 export * from './components/ui/navigation-menu'
 export {
@@ -78,76 +87,79 @@ export {
 } from '@/components/ui/input-otp'
 
 // extend
-export {Alert} from "./components/uix/alert";
-export type {AlertProps} from './components/uix/alert';
-export {Divider} from "./components/uix/divider";
-export type {DividerProps} from './components/uix/divider';
-export {Button} from './components/uix/button';
-export type {ButtonProps} from './components/uix/button';
-export {Form, FormItem} from './components/uix/form';
-export type {RenderProps, FieldItem, FormProps} from './components/uix/form';
-export {useMessage} from './components/uix/message';
-export {Steps, StepsItem} from './components/uix/steps';
-export type {StepsProps, StepsItemProps} from './components/uix/steps';
-export {Image} from './components/uix/image';
-export type {ImageProps} from './components/uix/image';
-export {Result} from './components/uix/result';
-export type {ResultProps} from './components/uix/result';
-export {Space} from './components/uix/space';
-export {Spin} from './components/uix/spin';
-export type {SpinProps} from './components/uix/spin';
-export {Avatar} from './components/uix/avatar';
-export type {AvatarProps} from './components/uix/avatar';
-export {Dropdown} from './components/uix/dropdown';
-export type {DropdownProps, DropdownMenuItemProps} from './components/uix/dropdown';
-export {DataTable} from './components/uix/data-table';
-export type {DataTableProps, DataTableColumn} from './components/uix/data-table';
-export {Filters} from './components/uix/filters';
-export type {FiltersProps, FilterItemProps} from './components/uix/filters';
-export {Select} from './components/uix/select';
-export type {SelectProps, SelectOptionProps} from './components/uix/select';
-export {Card} from './components/uix/card';
-export type {CardProps} from './components/uix/card';
-export {Action} from './components/uix/action';
-export type {ActionProps} from './components/uix/action';
-export {Breadcrumbs, BreadcrumbsItem} from './components/uix/breadcrumbs';
-export type {BreadcrumbsProps, BreadcrumbsItemProps} from './components/uix/breadcrumbs';
-export {Dialog} from './components/uix/dialog';
-export type {DialogProps} from './components/uix/dialog';
-export {ComboSelect} from './components/uix/combo-select';
-export type {ComboSelectProps, ComboSelectOptionProps} from './components/uix/combo-select';
-export {Checkbox} from "./components/uix/checkbox";
-export type {CheckboxProps} from "./components/uix/checkbox";
-export {CheckboxGroup} from "./components/uix/checkbox-group";
-export type {CheckboxGroupProps, CheckboxGroupOptionProps} from "./components/uix/checkbox-group";
-export {SimpleRadioGroup} from "./components/uix/radio-group";
-export type {SimpleRadioGroupProps, SimpleRadioGroupOptionProps} from "./components/uix/radio-group";
-export {Switch} from "./components/uix/switch";
-export type {SwitchProps} from "./components/uix/switch";
-export {Loading} from "./components/uix/loading";
-export type {LoadingProps} from "./components/uix/loading";
-export {ValueFormatter, register} from "./components/uix/value-formatter";
-export type {ValueFormatterProps} from "./components/uix/value-formatter";
-export {Pagination} from "./components/uix/pagination";
-export type {PaginationProps} from "./components/uix/pagination";
-export {ConfigProvider} from "./components/uix/config-provider";
-export type {ConfigProviderProps} from "./components/uix/config-provider";
-export {useAlert} from "./components/uix/alert-dialog";
-export type {ConfirmProps} from "./components/uix/alert-dialog";
-export {DateRangePicker} from "./components/uix/date-range-picker";
-export type {DateRangePickerProps} from "./components/uix/date-range-picker";
-export {Uploader} from "./components/uix/uploader";
-export type {UploaderProps} from "./components/uix/uploader";
+export { Alert } from './components/uix/alert'
+export type { AlertProps } from './components/uix/alert'
+export { Divider } from './components/uix/divider'
+export type { DividerProps } from './components/uix/divider'
+export { Button } from './components/uix/button'
+export type { ButtonProps } from './components/uix/button'
+export { Form, FormItem } from './components/uix/form'
+export type { RenderProps, FieldItem, FormProps } from './components/uix/form'
+export { useMessage } from './components/uix/message'
+export { Steps, StepsItem } from './components/uix/steps'
+export type { StepsProps, StepsItemProps } from './components/uix/steps'
+export { Image } from './components/uix/image'
+export type { ImageProps } from './components/uix/image'
+export { Result } from './components/uix/result'
+export type { ResultProps } from './components/uix/result'
+export { Space } from './components/uix/space'
+export { Spin } from './components/uix/spin'
+export type { SpinProps } from './components/uix/spin'
+export { Avatar } from './components/uix/avatar'
+export type { AvatarProps } from './components/uix/avatar'
+export { Dropdown } from './components/uix/dropdown'
+export type { DropdownProps, DropdownMenuItemProps } from './components/uix/dropdown'
+export { DataTable } from './components/uix/data-table'
+export type { DataTableProps, DataTableColumn } from './components/uix/data-table'
+export { Filters } from './components/uix/filters'
+export type { FiltersProps, FilterItemProps } from './components/uix/filters'
+export { Select } from './components/uix/select'
+export type { SelectProps, SelectOptionProps } from './components/uix/select'
+export { Card } from './components/uix/card'
+export type { CardProps } from './components/uix/card'
+export { Action } from './components/uix/action'
+export type { ActionProps } from './components/uix/action'
+export { Breadcrumbs, BreadcrumbsItem } from './components/uix/breadcrumbs'
+export type { BreadcrumbsProps, BreadcrumbsItemProps } from './components/uix/breadcrumbs'
+export { Dialog } from './components/uix/dialog'
+export type { DialogProps } from './components/uix/dialog'
+export { ComboSelect } from './components/uix/combo-select'
+export type { ComboSelectProps, ComboSelectOptionProps } from './components/uix/combo-select'
+export { Checkbox } from './components/uix/checkbox'
+export type { CheckboxProps } from './components/uix/checkbox'
+export { CheckboxGroup } from './components/uix/checkbox-group'
+export type { CheckboxGroupProps, CheckboxGroupOptionProps } from './components/uix/checkbox-group'
+export { SimpleRadioGroup } from './components/uix/radio-group'
+export type { SimpleRadioGroupProps, SimpleRadioGroupOptionProps } from './components/uix/radio-group'
+export { Switch } from './components/uix/switch'
+export type { SwitchProps } from './components/uix/switch'
+export { Loading } from './components/uix/loading'
+export type { LoadingProps } from './components/uix/loading'
+export { ValueFormatter, register } from './components/uix/value-formatter'
+export type { ValueFormatterProps } from './components/uix/value-formatter'
+export { Pagination } from './components/uix/pagination'
+export type { PaginationProps } from './components/uix/pagination'
+export { ConfigProvider } from './components/uix/config-provider'
+export type { ConfigProviderProps } from './components/uix/config-provider'
+export { useAlert } from './components/uix/alert-dialog'
+export type { ConfirmProps } from './components/uix/alert-dialog'
+export { DateRangePicker } from './components/uix/date-range-picker'
+export type { DateRangePickerProps } from './components/uix/date-range-picker'
+export { Uploader } from './components/uix/uploader'
+export type { UploaderProps } from './components/uix/uploader'
 export type { UploadFile, HandleProps } from './components/uix/uploader/type'
-export {DatePicker} from "./components/uix/date-picker";
-export type {DatePickerProps} from "./components/uix/date-picker";
-export {Tooltip} from './components/uix/tooltip';
-export type {TooltipProps} from './components/uix/tooltip';
-export {Empty} from './components/uix/empty';
-export type {EmptyProps} from './components/uix/empty';
-export {Tree} from "./components/uix/tree";
-export type {TreeProps, TreeData} from "./components/uix/tree";
-export {TreeSelect} from "./components/uix/tree-select";
-export type {TreeSelectProps} from "./components/uix/tree-select";
-export {TreeTable} from "./components/uix/tree-table";
-export type {TreeTableColumn, TreeTableProps} from "./components/uix/tree-table";
+export { DatePicker } from './components/uix/date-picker'
+export type { DatePickerProps } from './components/uix/date-picker'
+export { Tooltip } from './components/uix/tooltip'
+export type { TooltipProps } from './components/uix/tooltip'
+export { Empty } from './components/uix/empty'
+export type { EmptyProps } from './components/uix/empty'
+export { Tree } from './components/uix/tree'
+export type { TreeProps, TreeData } from './components/uix/tree'
+export { TreeSelect } from './components/uix/tree-select'
+export type { TreeSelectProps } from './components/uix/tree-select'
+export { TreeTable } from './components/uix/tree-table'
+export type { TreeTableColumn, TreeTableProps } from './components/uix/tree-table'
+
+export { BroadcastChannelProvider, useBroadcastChannel } from './components/uix/broadcast-channel-context'
+export type { BroadcastChannelProviderProps } from './components/uix/broadcast-channel-context'
