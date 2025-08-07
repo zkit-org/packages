cf21f59e45b172670f0e0dfb4604c9a5ec44b7a5
Grant <284885069@qq.com>
2025-08-07 18:46:59 +0800

    feat: Divider add borderClassName

--- packages/design/src/components/uix/divider/index.tsx
+++ packages/design/src/components/uix/divider/index.tsx
@@ -5,13 +5,14 @@ export interface DividerProps extends PropsWithChildren {
   orientation: 'left' | 'center' | 'right'
   className?: string
   labelClassName?: string
+  borderClassName?: string
 }
 
 export const Divider = (props: DividerProps) => {
   return (
     <div className={cn('relative my-6', props.className)}>
       <div className="absolute inset-0 flex items-center">
-        <span className="w-full border-t" />
+        <span className={cn('w-full border-t', props.borderClassName)} />
       </div>
       <div className="relative flex justify-center text-xs uppercase">
         <span className={cn('bg-background px-2 text-muted-foreground', props.labelClassName)}>{props.children}</span>
