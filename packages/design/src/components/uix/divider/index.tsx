ed4c93926ccebd4985df3c3a4c95faa4b23605f8
Grant <284885069@qq.com>
2025-08-07 18:42:01 +0800

    feat: Divider add className

--- packages/design/src/components/uix/divider/index.tsx
+++ packages/design/src/components/uix/divider/index.tsx
@@ -1,17 +1,20 @@
 import type { PropsWithChildren } from 'react'
+import { cn } from '@/lib/utils'
 
 export interface DividerProps extends PropsWithChildren {
-  orientation: 'left' | 'center' | 'right';
+  orientation: 'left' | 'center' | 'right'
+  className?: string
+  labelClassName?: string
 }
 
 export const Divider = (props: DividerProps) => {
   return (
-    <div className="relative my-6">
+    <div className={cn('relative my-6', props.className)}>
       <div className="absolute inset-0 flex items-center">
         <span className="w-full border-t" />
       </div>
       <div className="relative flex justify-center text-xs uppercase">
-        <span className="bg-background px-2 text-muted-foreground">{props.children}</span>
+        <span className={cn('bg-background px-2 text-muted-foreground', props.labelClassName)}>{props.children}</span>
       </div>
     </div>
   )
